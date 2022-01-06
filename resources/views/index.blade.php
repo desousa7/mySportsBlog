7885206744fdd28f6c60c9d9e6e2d5b6004d5941
desousa7 <74316631+desousa7@users.noreply.github.com>
2022-01-06 12:51:21 +0000

    home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 
 @section ('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16sm:m-auto w-3/5 block text-center">
+                <h1 class="sm:text-white text-6xl uppercase font-bold text-shadow-md pb-14">
+                    About
+                </h1>
+                <p class="text-center text-white-700 py-2 px-4 font-bold text-xl uppercase">
+                    This blog has been designed and created to bring all sports lovers together and create a great
+                    community. There will be loads of sports content from around the world and and a chance for us
+                    all to share our views and also have some friendly-banter.
+                </p>
+            </div>
+        </div>
+    </div>
+
 
 
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-gray-800 py-20 mt-20">
-    <div class="sm:grid grid-cols-3 w-25 pb-10 m-auto border-b-2 border-gray-700">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
         <div>
             <h3 class="text-l sm:font-bold text-gray-400">
                 Pages
@@ -83,7 +83,7 @@
         </div>
 
     </div>
-    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-grey-100 pt-6">
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
         Copyright 2021-2022 My Sports Blog. All Rights Reserved.
     </p>
 </footer>
