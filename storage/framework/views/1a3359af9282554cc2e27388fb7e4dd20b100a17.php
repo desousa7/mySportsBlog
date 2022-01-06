b020e1c5ebdf142e64362aba02a78ee02454e222
desousa7 <74316631+desousa7@users.noreply.github.com>
2022-01-06 11:55:17 +0000

    change1

---
+++ storage/framework/views/1a3359af9282554cc2e27388fb7e4dd20b100a17.php
@@ -0,0 +1,89 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-25 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-400">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Login
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-400">
+                Find Me
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        About me
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Email
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Instagram
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-400">
+                Latest posts
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Manchester United Slump...agian!
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Liverpool Covid-19 outbreak
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Endland Ashes debacle!
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Peter 'Snake-Bite' Wright world champ once more!
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-grey-100 pt-6">
+        Copyright 2021-2022 My Sports Blog. All Rights Reserved.
+    </p>
+</footer><?php /**PATH C:\xampp\htdocs\mySportsBlog\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
