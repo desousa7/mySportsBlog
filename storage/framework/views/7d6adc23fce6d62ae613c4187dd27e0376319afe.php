938e6178ce1b6c3001e818935d09b75e13230bf2
desousa7 <74316631+desousa7@users.noreply.github.com>
2022-01-08 18:18:06 +0000

    changes

---
+++ storage/framework/views/7d6adc23fce6d62ae613c4187dd27e0376319afe.php
@@ -0,0 +1,30 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="background-image2 grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="background-color-gray m-auto pt-4 pb-16sm:m-auto w-3/5 block text-center">
+                <h1 class="sm:text-white text-6xl uppercase font-bold text-shadow-md pb-14">
+                    Contacts
+                </h1>
+                <h3 class="text-center text-white-700 py-2 px-4 font-bold text-xl uppercase">
+                    Email    
+                </h3>
+                <h1>
+                    desousa.reuben7@gmail.com
+                </h1>
+                <h3 class="text-center text-white-700 py-2 px-4 font-bold text-xl uppercase">
+                    Instagram    
+                </h3>
+                <h1>
+                    rdesousa7
+                </h1>
+            </div>
+        </div>
+    </div>
+
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\mySportsBlog\resources\views/contacts.blade.php ENDPATH**/ ?>
