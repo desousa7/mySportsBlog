d88b5de284bf476317ad1702baa3f5d97aaec5b8
desousa7 <74316631+desousa7@users.noreply.github.com>
2022-01-08 19:19:31 +0000

    upload button

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -42,16 +42,10 @@
         <br>
 
         <div class="bg-grey-lighter pt-15">
-            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide
-            uppercase border border-blue cursor-pointer">
-                <span class="mt-2 text-base leading-normal">
-                    Select a File 
-                </span>
-                <input
-                    type="file"
-                    name="image"
-                    class="hidden">
-            </label>
+            <input
+                type="file"
+                name="image"
+                class="block">
         </div>
         <button 
             type="submit"
